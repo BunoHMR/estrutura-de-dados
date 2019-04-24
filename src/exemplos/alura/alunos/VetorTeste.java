cba26e0b878575667b0bfc2d5d87214a2c603b54
alexrebello <alexrebello@ciandt.com>
2019-04-24 18:49:55 -0300

    refactor das pastas

--- src/exemplos/alura/alunos/VetorTeste.java
+++ src/exemplos/alura/alunos/VetorTeste.java
@@ -1,13 +1,13 @@
-package exemplos.alura.alunos;;
+package exemplos.alura.alunos;
 
 public class VetorTeste {
 
     public static void main(final String[] args) {
-        Aluno a1 = new Aluno("Joao");
-        Aluno a2 = new Aluno("Maria");
-        Aluno a3 = new Aluno("Danilo");
-        Aluno a4 = new Aluno("Marcio");
-        Aluno a5 = new Aluno("Phill");
+        final Aluno a1 = new Aluno("Joao");
+        final Aluno a2 = new Aluno("Maria");
+        final Aluno a3 = new Aluno("Danilo");
+        final Aluno a4 = new Aluno("Marcio");
+        final Aluno a5 = new Aluno("Phill");
 
         final Vetor lista = new Vetor();
 
@@ -22,18 +22,17 @@ public class VetorTeste {
         System.out.println(lista.length());
 
         System.out.println(lista);
-        
+
         System.out.println(lista.contains(a1));
-        
+
         System.out.println(lista.contains(a3));
-        
+
         //listar a pessoa na posicao
         System.out.println(lista.get(2));
-        
-        
-        lista.add(a5, 2);
+
+        lista.add(a5);
         System.out.println(lista);
-        
+
         lista.remove(1);
         System.out.println(lista);
     }
